f5b987155de65c7d261f17cdc99c65baafd4cbce
imaneroucdhi <imane.rcdi@gmail.com>
2022-11-15 16:55:24 +0100

    validation form on javascript

--- registration.php
+++ registration.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<!-- @include 'config.php';
+if(isset($_POST['submit'])){
+    $name=mysqli_real_escape_string($_conn,$_POST['name']);
+    $email=mysqli_real_escape_string($_conn,$_POST['email']);
+    $password1=md5($_POST['password1']);
+    $password2=md5($_POST['password2']);
+} -->
+<?php
+?>
+
+
+<!-- <!DOCTYPE html> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -23,31 +34,78 @@
                         <div class="form-group mb-3">
                             <label for="exampleInputEmail1" class="form-label" id="username">Username</label>
                             <input type="text" class="form-control form-control-sm rounded-pill"  name="username" id="username" aria-describedby="emailHelp">
+                            <div class="error"></div>
                         </div>
                         <div class="form-group mb-3">
                             <label for="exampleInputPassword1" class="form-label">Email</label>
-                            <input type="email" name="email" class="form-control rounded-pill" id="email">
+                            <input type="email" name="email"  class="form-control rounded-pill" id="email">
+                            <div class="error"></div>
                         </div>
                         <div class="form-group mb-3">
                             <label class="form-check-label" for="exampleCheck1">Password</label> 
                             <input type="password" class="form-control rounded-pill" id="password1">
+                            <div class="error"></div>
                         </div>
                         <div class="form-group mb-3">
-                        <label class="form-check-label" for="exampleCheck1">Password again</label> 
+                            <label class="form-check-label" for="exampleCheck1">Password again</label> 
                             <input type="password" class="form-control rounded-pill" id="password2">
+                            <div class="error"></div>
                         </div>
-                        <button type="submit" class="btn mb-3 rounded-pill text-black">Sign In</button>
-                        <div class="signup ">
-                            Don't have an account? <a href="#" class="link-secondary">Create One</a>
+                        <div class="form-group mb-3">
+                            <button type="submit" class="btn mb-3 rounded-pill text-black">Sign In</button>
+                            <div class="signup ">
+                                Don't have an account? <a href="#" class="link-secondary">Create One</a>
+                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-    <!-- <a href="http://" style="float: right;font: size 12px;">Forget password</a> -->
+     <a href="http://" style="float: right;font: size 12px;">Forget password</a> 
     
 <script src="assets/js/script.js"></script>
 <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Validation</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styleRegistration.css">
+    <script defer src="assets/js/script.js"></script>
+</head>
+<body>
+    <div class="container">
+        <form id="form" action="/">
+            <h1>Registration</h1>
+            <div class="input-control">
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text">
+                <div class="error"></div>
+            </div>
+            <div class="input-control">
+                <label for="email">Email</label>
+                <input id="email" name="email" type="text">
+                <div class="error"></div>
+            </div>
+            <div class="input-control">
+                <label for="password">Password</label>
+                <input id="password"name="password" type="password">
+                <div class="error"></div>
+            </div>
+            <div class="input-control">
+                <label for="password2">Password again</label>
+                <input id="password2"name="password2" type="password">
+                <div class="error"></div>
+            </div>
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>
 </body>
 </html>
